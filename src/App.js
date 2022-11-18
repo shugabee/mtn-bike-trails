1429bb9be02878c4289d83179e37df2141972ea6
Shelby Webb <shelbylyn.webb@gmail.com>
2022-11-18 16:50:14 -0700

    first

--- src/App.js
+++ src/App.js
@@ -3,19 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import routes from "./routes";
 import "./App.css";
 
-// function setToken(userToken) {
-//   sessionStorage.setItem("token", JSON.stringify(userToken));
-// }
 
-// function getToken() {}
 
 function App() {
-  // const token = getToken();
-
-  // if (!token) {
-  //   return <LogIn setToken={setToken} />;
-  // }
-
+  
 return (
   <div className="App">
     {routes}
